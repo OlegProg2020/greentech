4d453916b3581bdc8ea73057a076446fcab65818
Oleg Minenko <justprog2020@yandex.ru>
2023-11-08 19:31:51 +0300

    create AccountService

--- greentech-backend/src/main/java/org/greentech/backend/service/AccountService.java
+++ greentech-backend/src/main/java/org/greentech/backend/service/AccountService.java
@@ -1,10 +1,13 @@
 package org.greentech.backend.service;
 
 import jakarta.validation.constraints.Min;
-import org.greentech.backend.entity.Account;
+import jakarta.validation.constraints.Pattern;
+import org.greentech.backend.dto.response.AccountResponseDto;
 
 public interface AccountService {
 
-    Account findById(@Min(value = 1, message = "id должен быть >= 1") Integer id);
+    AccountResponseDto findById(@Min(value = 1, message = "id должен быть >= 1") Integer id);
 
+    AccountResponseDto findByPhone(@Pattern(regexp = "^(?:\\+7|8)[0-9]{10}$",
+            message = "Телефон должен начинаться с 8 или +7 и продолжаться 10-ю цифрами") String phone);
 }
