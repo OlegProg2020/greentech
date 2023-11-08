9204073d3cede863dd9bea493c7f1e48396e3d20
Oleg Minenko <justprog2020@yandex.ru>
2023-11-08 16:44:30 +0300

    init project

---
+++ greentech-backend/src/main/java/org/greentech/backend/GreentechBackendApplication.java
@@ -0,0 +1,13 @@
+package org.greentech.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreentechBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreentechBackendApplication.class, args);
+    }
+
+}
---
+++ greentech-backend/src/test/java/org/greentech/backend/GreentechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greentech.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreentechBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
