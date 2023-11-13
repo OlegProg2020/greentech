0b5e10289c232f2bd9a8562dd0376e791f6587a7
Oleg Minenko <justprog2020@yandex.ru>
2023-11-13 19:24:05 +0300

    create search method in ProductService

--- greentech-backend/src/main/java/org/greentech/backend/data/repository/ProductRepository.java
+++ greentech-backend/src/main/java/org/greentech/backend/data/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package org.greentech.backend.data.repository;
 
 import org.greentech.backend.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findByNameLikeIgnoreCase(@Nullable String name, Pageable pageable);
 }
